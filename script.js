2b17f6418761860da47fb4bf00131c08dbfa2037
amenhasfaw <105349999+amenhasfaw@users.noreply.github.com>
2023-03-11 18:09:20 +0530

    Camera accessed and Canvas intialized

--- script.js
+++ script.js
@@ -7,33 +7,31 @@ const getVideo = () => {
 
     navigator.mediaDevices.getUserMedia({video:true}).then(vidStream => {
         video = document.createElement('video')
-        console.log(video)
         video.srcObject = vidStream;
         video.play()
 
         video.onloadedmetadata = (e) => {
-                updateCanvas()
+                updateCanvas()      
          };
         
     }).catch((e) => {
-        console.log(e.message)
         if(e.message === 'Permission denied'){
             const errorMsg = document.createElement('div')
             errorMsg.classList.add('Error')
             errorMsg.innerHTML = `
                 <h1>ERROR! PERMISSION DENIED</h1>
             `
-
             document.querySelector('body').innerHTML = ''
             document.querySelector('body').appendChild(errorMsg)
         }
     })
 }
 
-getVideo()
 
 const updateCanvas = () => {
     ctx.drawImage(video,0,0)
     window.requestAnimationFrame(updateCanvas)
 }
+
+getVideo()
 
